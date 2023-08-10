2cfaf6af2b09feda7c1af7a0e432515abff43d03
Dimitar Dalev <dpdalev@abv.bg>
2023-08-10 14:53:21 +0300

    Initial: basic assets and logic

---
+++ start.js
@@ -0,0 +1,41 @@
+const params = {
+    rotation: 0,
+    speed: 5,
+    angle: 0,
+    sectors: [
+        14,
+        10,
+        3,
+        14,
+        9,
+        17,
+        5,
+        14,
+        9,
+        11
+    ]
+}
+
+start = () => {
+
+    let index = 0;
+
+    document.getElementById("startButton").addEventListener("click", () => {
+        rotate(index);
+        index++
+    })
+}
+
+rotate = (index) => {
+    params.angle = params.sectors[index] / 18 * 180 + params.angle
+
+    console.log(index, params.sectors[index], params.angle)
+
+    gsap.to("#wheel", {
+        rotation: params.angle,
+        duration: 5,
+    });
+}
+
+window.onload = start();
+
